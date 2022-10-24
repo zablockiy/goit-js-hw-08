d94466be44a53f09d4cd5437b77bd2fd3418e871
zablockiy <105744371+zablockiy@users.noreply.github.com>
2022-10-24 12:18:45 +0300

    01-gallery.js done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+const imgGalleryEl = document.querySelector('.gallery');
+const markupGallery = createMarkUp(galleryItems);
+imgGalleryEl.innerHTML = markupGallery;
+imgGalleryEl.addEventListener('click', onClickImg);
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+function createMarkUp(galleryItems) {
+  return galleryItems
+    .map(item => {
+      return `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`;
+    })
+    .join('');
+}
+function onClickImg(event) {
+  event.preventDefault();
+}
