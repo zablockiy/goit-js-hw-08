63e6914de7ecfc7c2b471d07358c3e5954e3b8b1
zablockiy <105744371+zablockiy@users.noreply.github.com>
2022-10-24 15:51:49 +0300

    03-feedback.js done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'throttleit';
 
+const inputDataForm = document.querySelector('.feedback-form');
+inputDataForm.addEventListener('input', throttle(onInputData, 500));
+inputDataForm.addEventListener('submit', formSubmit);
+
+const KEY_DATA = 'feedback-form-state';
+const data = {};
+savedFormData();
+function onInputData(event) {
+  data[event.target.name] = event.target.value;
+  localStorage.setItem(KEY_DATA, JSON.stringify(data));
+  
+}
+function savedFormData() {
+  const saveData = JSON.parse(localStorage.getItem(KEY_DATA));
+  if (saveData) {
+    const inputEl = inputDataForm.querySelector('input');
+    const textareaEl = inputDataForm.querySelector('textarea');
+    inputEl.value = saveData.email ? saveData.email : '';
+    
+    textareaEl.value = saveData.message ? saveData.message : '';
+    data.email = saveData.email;
+    data.message = saveData.message;
+  }
+}
+
+function formSubmit(event) {
+  event.preventDefault();
+  if (data.email && data.message) {
+    data.email = '';
+    data.message = '';
+    event.currentTarget.reset();
+    localStorage.removeItem(KEY_DATA);
+  }
+}
