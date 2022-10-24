4d8bb28739c318aa5c47e25360f37a2e2a03ab28
zablockiy <105744371+zablockiy@users.noreply.github.com>
2022-10-24 12:56:31 +0300

    02-video.js done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import vimeoPlayer from '@vimeo/player';
+import throttle from 'throttleit';
+const iframe = document.querySelector('#vimeo-player');
+const player = new vimeoPlayer(iframe);
+setCurrentTime();
 
+player.on('timeupdate', throttle(pauseTime,1000));
+
+function pauseTime(data) {
+  localStorage.setItem('videoplayer-current-time', `${data.seconds}`);
+}
+function setCurrentTime() {
+  const timeToPause = localStorage.getItem('videoplayer-current-time');
+  if (timeToPause) {
+    player.setCurrentTime(timeToPause);
+  }
+}
